a1b805d75081540f8d7206982ded8fd86dfeafe2
SofiaDenisse <nsofiadenisse@gmail.com>
2026-04-13 22:20:43 -0600

    Fix: correct base path to PPCripto

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/paginapersonal/',
+  base: '/PPCripto/',
 })
